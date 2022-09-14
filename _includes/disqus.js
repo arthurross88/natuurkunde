7439995eda71dda5e2459e2142d9466ceac1fb35
arthurross88 <arthurross@live.nl>
2022-09-14 20:19:16 +0200

    formule

--- _includes/disqus.js
+++ _includes/disqus.js
@@ -8,7 +8,7 @@ var disqus_config = function () {
   // https://disqus.com/admin/universalcode
   (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
-    s.src = '//@disqus_35fSLp5UXv.disqus.com/embed.js';
+    s.src = '//disqus_35fSLp5UXv.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
   })();
